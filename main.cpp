fb18a697b1e28dcc190872dadbf9efc2f5dde8a4
Fernando Gómez <ferchogomez545@gmail.com>
2023-08-10 22:15:08 +0000

    agregado el mcm

--- main.cpp
+++ main.cpp
@@ -9,14 +9,16 @@
 #define BLINKING_PERIOD     20
 #define BLINKING_UNITS      "ms"
 #define BLINKING_RATE       20ms    // Falta concatenación de BLINKING_PERIOD y BLINKING_UNITS 
-// Periodos de parpadeo adimensionalizados de los LEDs: Tx = LEDx_PERIOD*BLINKING_RATE 
-#define LEDR_PERIOD         5       
-#define LEDG_PERIOD         7       
-#define LEDB_PERIOD         11      
+// Periodos de parpadeo adimensionalizados de los LEDs: Tx = LEDx_PERIOD * BLINKING_RATE 
+#define LEDR_PERIOD         5
+#define LEDG_PERIOD         7
+#define LEDB_PERIOD         11
 // Objeto para establecer la comunicación serial con el Pc
 UnbufferedSerial serial(USBTX, USBRX, 9600);
 using namespace std;
 
+int mcm(int a, int b);
+int mcm(int a, int b, int c);
 
 int main()
 {
@@ -26,22 +28,56 @@ int main()
     DigitalOut ledB(LED3, 1);
     // Variable contador - periodos del mcm de los LEDs
     int count = 0;
+    int ledr_period;
+    int ledg_period;
+    int ledb_period;
+
+    cout << "Por favor ingrese el periodo de parpadeo del led rojo" << endl;
+    cin >> ledr_period;
+    cout << "Usted ingreso" << ledr_period << endl;
+
+    cout << "Por favor ingrese el periodo de parpadeo del led verde" << endl;
+    cin >> ledg_period;
+    cout << "Usted ingreso" << ledg_period << endl;
+
+    cout << "Por favor ingrese el periodo de parpadeo del led azul" << endl;
+    cin >> ledb_period;
+    cout << "Usted ingreso" << ledb_period << endl;
+
+    int blinky = mcm(ledr_period, ledb_period, ledg_period);
+
+    cout << blinky << endl;
 
     while (true) {
-        if(count % 5 == 0) {
+        if(count % ledr_period == 0) {
             ledR = !ledR;
-            cout << "LedR = " << count*BLINKING_PERIOD << BLINKING_UNITS << endl;
+            cout << "LedR = " << count * BLINKING_PERIOD << BLINKING_UNITS << endl;
         }
-        if(count % 7 == 0) {
+        if(count % ledg_period == 0) {
             ledG = !ledG;
-            cout << "LedG = " << count*BLINKING_PERIOD << BLINKING_UNITS << endl;
+            cout << "LedG = " << count * BLINKING_PERIOD << BLINKING_UNITS << endl;
         }
-        if(count % 11 == 0) {
+        if(count % ledb_period == 0) {
             ledB = !ledB;
-            cout << "LedB = " << count*BLINKING_PERIOD << BLINKING_UNITS << endl;
+            cout << "LedB = " << count * BLINKING_PERIOD << BLINKING_UNITS << endl;
         }
 
-        ThisThread::sleep_for(BLINKING_RATE);
+        ThisThread::sleep_for(static_cast<uint32_t>(blinky));
         count += 1;
     }
+}
+
+int mcm(int a, int b) {
+    if (b == 0)
+        return a;
+    return mcm(b, a % b);
+}
+
+// Function to calculate the least common multiple (LCM) of three integers
+int mcm(int a, int b, int c) {
+    int gcd_of_ab = mcm(a, b);
+    int gcd_of_abc = mcm(gcd_of_ab, c);
+
+    int lcm = (a * b * c) / gcd_of_abc;
+    return lcm;
 }
